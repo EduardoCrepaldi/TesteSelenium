9967677a3cd15f100307b9bc9085ed23a5151dd6
EduardoCrepaldi <educrep@live.com>
2018-10-14 19:56:00 -0300

    [update] exemplo

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            IWebDriver chrome = new ChromeDriver();
+            chrome.Manage().Window.Maximize();
+            chrome.Url = "https://www.google.com.br/?gws_rd=ssl";
+
+
+        }
+    }
+}
